9e504759de6555227329df0f744499907fdfe8d6
Emillyn <emillyn_muniz@hotmail.com>
2022-01-18 21:48:01 -0300

    Create middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,75 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => (user.username == username));
+
+  if(!user){
+    return response.status(404).json({error: "Username already exists!"});
+  }
+
+  request.user = user
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro == false && user.todos.length == 10) return response.status(403);
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(
+      user => user.username === username
+  );
+
+  if (!user) {
+      return response.status(404).json({
+          error: "User not found"
+      });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: "todo not found"
+    })
+  }
+
+  const todo = user.todos.find(
+    todo => todo.id === id
+  )
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "todo not found"
+    })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => (user.id == id));
+
+  if(!user){
+    return response.status(404).json({error: "Username is exists!"});
+  }
+
+  request.user = user
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
